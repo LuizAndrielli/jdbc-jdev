76037d92cbf097be97d182ea05d9b7f732f82151
Luiz Augusto <gutoandrielli@gmail.com>
2024-03-29 19:21:39 -0300

    Hello world, adicionado dependencia do PostGre

---
+++ src/main/java/pos_java_jdbc/jdbc/App.java
@@ -0,0 +1,13 @@
+package pos_java_jdbc.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World pos java" );
+    }
+}
