0ed7f114f00c1f4b4c4d3a1c22f9b6745459eb59
Luiz Augusto <gutoandrielli@gmail.com>
2024-03-29 23:37:02 -0300

    Criando nosso INSERT ao banco de dados

--- src/main/java/dao/UserPostDao.java
+++ src/main/java/dao/UserPostDao.java
@@ -1,8 +1,10 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import conexaojdbc.SingleConnection;
+import model.Userpostjava;
 
 public class UserPostDao {
 	
@@ -10,6 +12,20 @@ public class UserPostDao {
 	
 	public UserPostDao() {
 		connection = SingleConnection.getConnection();
+	}
+	
+	public void salvar(Userpostjava userpostjava) {
+		try {
+			String sql = "insert into userpostjava(id,nome,email) values (?,?,?)";
+			PreparedStatement insert = connection.prepareStatement(sql);
+			insert.setLong(1, userpostjava.getId());
+			insert.setString(2, userpostjava.getNome());
+			insert.setString(3, userpostjava.getEmail());
+			insert.execute();
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/test/java/pos_java_jdbc/jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/jdbc/TesteBancoJdbc.java
@@ -2,13 +2,23 @@ package pos_java_jdbc.jdbc;
 
 import org.junit.Test;
 
-import conexaojdbc.SingleConnection;
+
+import dao.UserPostDao;
+import model.Userpostjava;
 
 public class TesteBancoJdbc {
 	
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserPostDao userPostDao = new UserPostDao();
+		Userpostjava userpostjava = new Userpostjava();
+		
+		userpostjava.setId(4L);
+		userpostjava.setNome("Junit Teste");
+		userpostjava.setEmail("teste@junit.com");
+		
+		userPostDao.salvar(userpostjava);
+		
 	}
 
 }
